48f739ee24c4f47099e8fd5eeb70bdf7de2ce03e
Balakrishnan G <balakrishnan@semcoisc.com>
2018-11-30 13:13:39 +0530

    'base'

---
+++ LD2.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import os; 
+print(os.environ.get('QT_API'))
+#%matplotlib qt
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('./camera_cal/*.jpg')
+#print(images)
+# Step through the list and search for chessboard corners
+index = 0
+for fname in images:
+    print(fname)
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('gray',gray)
+    #cv2.waitKey(0)
+    
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+    # If found, add object points, image points
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (9,6), corners, ret)
+        #cv2.imshow('img',img)
+        #cv2.waitKey(30)
+        cv2.imwrite('./camera_cal/out/'+str(index)+'.png',img)
+    index+=1
+#cv2.destroyAllWindows()
+    
